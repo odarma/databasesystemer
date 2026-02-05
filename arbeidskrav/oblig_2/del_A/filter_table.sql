0166569c02106ea71f8c85a2b1cc40f0acbedd98
omid <227144302+odarma@users.noreply.github.com>
2026-02-05 15:58:43 +0100

    .

--- arbeidskrav/oblig_2/del_A/filter_table.sql
+++ arbeidskrav/oblig_2/del_A/filter_table.sql
@@ -1,3 +1,7 @@
 select fornavn, etternavn from pasient where allergier="null";
 select fornavn, etternavn,kjoenn from pasient where kjoenn="M";
-select fornavn from pasient where fornavn="C";
+select fornavn from pasient where fornavn like 'C%';
+select fornavn, etternavn from pasient where vekt between 100 and 120;
+update pasient set allergier='NKA' where allergier='null';
+select * from pasient where allergier='NKA';
+select concat(fornavn,' ', etternavn) as fullt_navn from pasient;
