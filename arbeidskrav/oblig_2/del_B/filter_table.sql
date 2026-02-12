b9b6c17411b4330fb1e35af1a6f8c50ef96a6e51
omid <227144302+odarma@users.noreply.github.com>
2026-02-12 18:53:36 +0100

    .

--- arbeidskrav/oblig_2/del_B/filter_table.sql
+++ arbeidskrav/oblig_2/del_B/filter_table.sql
@@ -5,3 +5,7 @@ select distinct sted,provins_id from pasienter where provins_id = "NS";
 select fornavn,etternavn,fodseldag from pasienter where hoyde>160 and vekt>70;
 select distinct year(fodseldag) as ar from pasienter order by ar asc;
 select distinct fornavn from pasienter;
+select id,fornavn from pasienter where fornavn like "S____%s";
+select fornavn,length(fornavn) as lengde from pasienter order by lengde desc, fornavn asc; 
+select fornavn,etternavn,allergier from pasienter where allergier = "Penicillin" or allergier = "Morphine" 
+order by allergier asc, fornavn asc, etternavn asc;
