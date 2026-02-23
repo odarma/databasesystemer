47935184c9403d8d1b1413759ea629d05ab6799b
omid <odarma@proton.me>
2026-02-23 13:59:13 +0100

    .

--- forelesning/scripts_for_garnDB/filter_tables.sql
+++ forelesning/scripts_for_garnDB/filter_tables.sql
@@ -1,1 +1,20 @@
-select * from garn where lopelengde>200 order by lopelengde;
+-- select * from garn where lopelengde>200 order by lopelengde;
+use garn;
+-- oppgave 1
+select garn.produsent_id,produsent.navn as produsent_navn,garn.navn as garn_navn, pris as orginal_pris, pris*1.1 as justert_pris 
+from garn inner join produsent on garn.produsent_id = produsent.id;
+select garn.produsent_id,produsent.navn as produsent_navn,garn.navn as garn_navn, pris as orginal_pris, pris*1.1 as justert_pris 
+from garn,produsent where produsent_id = produsent.id;
+
+-- oppgave 2
+select navn, round(pris,-1) as avrundet_pris_tier from garn;
+
+-- oppgave 3
+select upper(produsent.navn) as produsent_navn,upper(garn.navn) as garn_navn from garn,produsent where produsent_id = produsent.id;
+
+-- oppgave 4
+select round(avg(pris),0) as snitt_pris from garn;
+
+-- oppgave 5
+select produsent_id, count(garn.navn) as antall_garn, round(avg(pris),0) as snitt_pris, min(pris) as minste_pris, max(pris) as høyeste_pris
+from garn GROUP BY produsent_id;
