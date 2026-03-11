884bbd12050c91dd9b91ba1b72953cea163efa17
omid <227144302+odarma@users.noreply.github.com>
2026-03-11 19:02:35 +0100

    .

--- arbeidskrav/oblig_2/del_D/filter_table.sql
+++ arbeidskrav/oblig_2/del_D/filter_table.sql
@@ -31,19 +31,36 @@ select p.id, fornavn, etternavn from pasienter p
 where p.id not in (select pasienter_id from innleggelser);
 
 -- oppgave 8
-
+select fornavn,etternavn,count(i.id) as antall_forekomster from pasienter p inner join innleggelser i on p.id=pasienter_id
+group by fornavn,etternavn having antall_forekomster>1;
 
 -- oppgave 9
-
+select pasienter_id, leger_id, diagnose from innleggelser where (leger_id in (1,5,19) 
+and mod(pasienter_id,2)>0) or (leger_id like "%2%" and pasienter_id like"___");
 
 -- oppgave 10
-
+select 
+    case 
+        when vekt < 150 then concat('vektgruppe ', floor(vekt / 10) * 10, '-', (floor(vekt / 10) * 10) + 9)
+        else 'vektgruppe 150+'
+    end as vektgruppe,
+    count(id) as antall
+from pasienter
+group by vektgruppe
+order by min(vekt) desc; -- sortere etter minstevekt for hver vektgruppe
 
 -- oppgave 11
-
+select id, vekt, hoyde, 
+case
+when vekt/power(hoyde/100,2) >= 30 then '1'
+else '0' 
+end as erOvervektig 
+from pasienter;
 
 -- oppgave 12
-
+select pasienter_id, l.fornavn as lege_fornavn, l.etternavn as lege_etternavn, spesialitet 
+from innleggelser i inner join leger l on leger_id = l.id where l.fornavn like "Lisa" 
+and diagnose like "Epilepsy";
 
 -- oppgave 13
 
